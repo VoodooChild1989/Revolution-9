86fc49ad3821ad3d4942c08e0a19ae1bf6aebb35
Nihil <diisisiwhjif@gmail.com>
2025-07-07 17:43:54 +0500

    Save

--- Code/JavaScript/buttons.js
+++ Code/JavaScript/buttons.js
@@ -7,3 +7,24 @@ function greet(name)
 {
     alert("Hello from External JS! " + name);
 }
+
+const observer = new IntersectionObserver
+(entries => 
+    {
+    entries.forEach(entry => 
+    {
+        if (entry.isIntersecting) 
+        {
+        entry.target.classList.add('active');
+        } 
+        else 
+        {
+        entry.target.classList.remove('active');
+        }
+    });
+    });
+
+document.querySelectorAll('.animate-on-scroll').forEach(el => 
+{
+    observer.observe(el);
+});
